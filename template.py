7fe13f49992457fcde26e6227281b3365954043a
Leax <41660941+leaxpm@users.noreply.github.com>
2023-02-07 21:48:51 -0600

    Update template.py

--- template.py
+++ template.py
@@ -1,1 +1,23 @@
+#! /usr/bin/python3
+from pwn import *
 
+#Connection host
+host = '127.0.0.1'
+puerto = 9003
+
+#Settings Linux 
+nombre_binario = './'
+binario = context.binary = ELF(nombre_binario)
+
+#DEBUG OPTION
+context.log_level = "debug"
+
+if(args['REMOTO']):
+    p = remote(host, puerto)
+else:
+    p = process(nombre_binario)
+
+if(args['GDB']):
+    comandos_gdb = ""
+    comandos_gdb += "continue\n"
+    gdb.attach(p, comandos_gdb)
